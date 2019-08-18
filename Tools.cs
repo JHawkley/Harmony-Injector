964d59bb6501769bee7e8a048b5fdb0ec3cf6f1f
James Dean <james@terradomain.us>
2019-08-18 04:47:05 -0500

    Improves exception reports.
    Now can also display custom exception information through `Exception#Data`.

--- Tools.cs
+++ Tools.cs
@@ -2,8 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
-using System.Reflection;
-using System.Runtime.CompilerServices;
+using System.Text;
 using System.Threading;
 
 using XRLCore = XRL.Core.XRLCore;
@@ -51,14 +50,31 @@ namespace HarmonyInjector
         /// </summary>
         /// <param name="exception">The exception to build a message for.</param>
         /// <returns>A log-friendly exception message.</returns>
-        public static string ToLogMessage(this Exception exception) => String.Join(
-            separator: "\r\n\r\n",
-            value : new string[]
+        public static string ToLogMessage(this Exception exception)
+        {
+            var sb = new StringBuilder();
+            sb.Append("Exception report...");
+            sb.AppendLine().AppendLine();
+            sb.Append(exception.ToString());
+
+            if (exception.Data.Count > 0)
             {
-                "Exception report...",
-                exception.ToString()
+                // List out the custom data stored in the exception.
+                sb.AppendLine().AppendLine();
+                sb.Append("Custom exception data:");
+
+                var de = exception.Data.GetEnumerator();
+                while (de.MoveNext())
+                {
+                    // Indent the data, if it has new-lines.
+                    var value = de.Value.ToString().Replace("\n", "\n    ");
+                    var data = $"  {de.Key.ToString()} => {value}";
+                    sb.AppendLine().Append(data);
+                }
             }
-        );
+
+            return sb.ToString();
+        }
 
         /// <summary>
         /// Enumerates an exception and all of its inner-exceptions.
